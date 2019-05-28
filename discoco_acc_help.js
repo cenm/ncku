0ea75e7c3b63ece8830102549a880d1a68663597
cenm <cenm.lin@gmail.com>
2019-05-28 13:59:43 +0800

    20190528re

--- discoco_acc_help.js
+++ discoco_acc_help.js
@@ -1,36 +1,24 @@
 var ds=getUrlVars()["ds"];
+var sds;
+switch(ds){
+case 'ds_shanhua_mp' :
+	sds = '1';
+	break;
+}
+
 $(document).ready(function () {
 	if(ds){
 		$("#mainmenu" ).hide();
 		$("#login_ui" ).hide();
-		
-		$("#title_mainblk").html('民眾抽獎註冊頁');
+		$(".title_mainblk").html('<div style="text-align: center;padding-top: 28px;">民眾抽獎註冊頁</div>');
 		switch(ds){
 		case 'ds_shanhua_mp' :
-			$("#discoco_text").html('歡迎參加善化商圈行動支付抽獎活動，請利用FACEBOOK或GOOGLE帳號登入');
-			changelisten();
+			$("#discoco_text").html('<div style="text-align: center;padding-top: 28px;">歡迎參加善化商圈行動支付抽獎活動，請利用FACEBOOK或GOOGLE帳號登入，並填寫聯絡資料，即可擁有抽獎資格</div>');
 			break;
+		default:
+			window.open('./',"_parent").focus();
 		}
 		
 	}
 });
-function changelisten(){
-	$(".btn_fb_login,.btn_gl_login").change(function(){
-		if($(".btn_fb_login").attr("onclick")=='FBLogout()'||$(".btn_gl_login").attr("onclick")=='GOOGLELogout()'){
-			reg_direct_open();
-		}else{
-			reg_direct_close();
-		}
-		
-	});
-}
-function reg_direct_open(){
-	$( "#register_user_detail_bk" ).load( "register_user_detail #register_user_body", function( response, status, xhr ) {
-		if ( xhr.status == "404" ) {
-			$( "#register_user_detail_bk" ).load( "register_user_detail.jsp #register_user_body");
-		}
-	});
-}
-function reg_direct_close(){
-	$( "#register_user_detail_bk" ).html("");
-}
+
