579057e63f4a069ac5b03cdcb464b6c24df9741a
cenm <cenm.lin@gmail.com>
2019-05-28 14:50:57 +0800

    renew

--- efstore_help.js
+++ efstore_help.js
@@ -2,6 +2,6 @@
 $(document).ready(function () {
 	var pagename = window.location.pathname.split('.jsp')[0].split('?')[0].split('/').pop();
 	console.log(pagename);
-	$('#register_shop_body').html();
+	$('#register_shop_body').html("");
 });
 
