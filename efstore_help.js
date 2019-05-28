37ca33f4177ad864be7704f82f1a26e1b8248b5e
cenm <cenm.lin@gmail.com>
2019-05-28 14:37:35 +0800

    renew

--- efstore_help.js
+++ efstore_help.js
@@ -1,24 +1,6 @@
-var ds=getUrlVars()["ds"];//
-var sds;
-switch(ds){
-case 'ds_shanhua_mp' :
-	sds = '1';
-	break;
-}
 
 $(document).ready(function () {
-	if(ds){
-		$("#mainmenu" ).hide();
-		$("#login_ui" ).hide();
-		$(".title_mainblk").html('<div style="text-align: center;padding-top: 28px;">民眾抽獎註冊頁</div>');
-		switch(ds){
-		case 'ds_shanhua_mp' :
-			$("#discoco_text").html('<div style="text-align: center;padding-top: 28px;">歡迎參加善化商圈行動支付抽獎活動，請利用FACEBOOK或GOOGLE帳號登入，並填寫聯絡資料，即可擁有抽獎資格</div>');
-			break;
-		default:
-			window.open('./',"_parent").focus();
-		}
-		
-	}
+	var pagename = window.location.pathname.split('.jsp')[0].split('?')[0].split('/').pop();
+	console.log(pagename);
 });
 
