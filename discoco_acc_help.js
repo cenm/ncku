6bc2a4825b854c25213a324bcf24e96bc323ce81
cenm <cenm.lin@gmail.com>
2019-05-29 13:34:30 +0800

    renew

--- discoco_acc_help.js
+++ discoco_acc_help.js
@@ -1,8 +1,11 @@
 var ds=getUrlVars()["ds"];//
 var sds;
 switch(ds){
-case 'ds_shanhua_mp' :
+case 'ds_all_mp' :
 	sds = '1';
+	break;
+case 'ds_shanhua_mp' :
+	sds = '2';
 	break;
 }
 
@@ -12,6 +15,9 @@ $(document).ready(function () {
 		$("#login_ui" ).hide();
 		$(".title_mainblk").html('<div style="text-align: center;padding-top: 28px;">民眾抽獎註冊頁</div>');
 		switch(ds){
+		case 'ds_all_mp' :
+			$("#discoco_text").html('<div style="text-align: center;padding-top: 28px;">歡迎參加臺南購便利行動支付So Easy抽獎活動，請利用FACEBOOK或GOOGLE帳號登入，並填寫聯絡資料，即可擁有抽獎資格</div>');
+			break;
 		case 'ds_shanhua_mp' :
 			$("#discoco_text").html('<div style="text-align: center;padding-top: 28px;">歡迎參加善化商圈行動支付抽獎活動，請利用FACEBOOK或GOOGLE帳號登入，並填寫聯絡資料，即可擁有抽獎資格</div>');
 			break;
