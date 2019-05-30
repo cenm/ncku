17cf23bc2f79426e52b36e627d4939d961a85d8a
cenm <cenm.lin@gmail.com>
2019-05-30 16:25:48 +0800

    renew

--- discoco_help.js
+++ discoco_help.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
 			+'<div style="margin:15px 0px;border-top:1px #888888 solid"></div>'
 			+'<input style="margin: 15px 10px 5px 10px;cursor: pointer" type="button" value="善化年中享樂購公版(列印)"  onclick="print_detail(\'02\')"/>'
 			+'<div id=\'qr02\'>'
-			+'<div class="qr02_css1" style="height:0;padding:0 0 141% 0;background:#ba3939 url(\'https://i.imgur.com/1qSsI2A.jpg\') 0 0 no-repeat;-moz-background-size: cover;-webkit-background-size: cover;-o-background-size: cover;background-size: cover;">'
+			+'<div class="qr02_css1" style="height:0;padding:0 0 141% 0;background:#ba3939 url(\'https://i.imgur.com/pYilDhg.jpg\') 0 0 no-repeat;-moz-background-size: cover;-webkit-background-size: cover;-o-background-size: cover;background-size: cover;">'
 			+'<div class="qr_img" width="80%" style="padding: 18.5% 30% 10% 30%;"></div>'
 			+'</div>'
 			+'</div>'
