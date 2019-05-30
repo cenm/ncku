b3a595a806112c4d965324b067c47b0621e66b20
cenm <cenm.lin@gmail.com>
2019-05-30 15:39:19 +0800

    renew

--- discoco_help.js
+++ discoco_help.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-	var pagename = window.location.pathname.split('.jsp')[0].split('?')[0].split('/').pop();
+	/*var pagename = window.location.pathname.split('.jsp')[0].split('?')[0].split('/').pop();
+	pagename = pagename.replace(/\./g,'').replace('page','').replace('action','');//this is for oliver style
+	if(pagename=='coco'||pagename=='discoco'){//this is for discoco style
+		//window.history.pushState(null,null,'./');
+		window.history.replaceState(null,null,'./');	
+	}*/
 	console.log(pagename);
 	//$('#register_shop_body').html("<div style=\"margin-bottom:20px;color:#CC0000;font-size:20px\">目前報名已截止，錄取名單公布於最新消息</div>");
 });
