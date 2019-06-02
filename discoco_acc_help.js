0b9db6f8e9c4786ca56b1bfec5e2d1a9e8dc28b1
cenm <cenm.lin@gmail.com>
2019-06-02 23:30:22 +0800

    renew

--- discoco_acc_help.js
+++ discoco_acc_help.js
@@ -22,13 +22,11 @@ $(document).ready(function () {
 			$(".title_mainblk").html('<div style="text-align: center;padding-top: 28px;">「臺南購便利，行動支付So Easy」抽獎註冊頁</div>');
 			$("#discoco_text").html('<div style="text-align: center;padding-top: 28px;">歡迎參加「臺南購便利，行動支付So Easy」抽獎活動，請利用GOOGLE帳號登入，並填寫聯絡資料，即可擁有抽獎資格</div>');
 			$("#submit").val('我要中大獎');
-			$(".btn_fb_login").hide();
 			break;
 		case 'ds_shanhua_mp' :
 			$(".title_mainblk").html('<div style="text-align: center;padding-top: 28px;">「善化年中享樂購，商圈送你出國趣」抽獎註冊頁</div>');
 			$("#discoco_text").html('<div style="text-align: center;padding-top: 28px;">歡迎參加「善化年中享樂購，商圈送你出國趣」抽獎活動，請利用GOOGLE帳號登入，並填寫聯絡資料，即可擁有抽獎資格</div>');
 			$("#submit").val('我要中大獎');
-			$(".btn_fb_login").hide();
 			break;
 		default:
 			window.open('./',"_parent").focus();
