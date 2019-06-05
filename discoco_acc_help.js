4c3631b70c61f4653d81d127d2dc973782e11eb5
cenm <cenm.lin@gmail.com>
2019-06-05 15:45:13 +0800

    renew

--- discoco_acc_help.js
+++ discoco_acc_help.js
@@ -22,12 +22,15 @@ $(document).ready(function () {
 			$(".title_mainblk").html('<div style="text-align: center;padding-top: 28px;">「臺南購便利，行動支付So Easy」抽獎註冊頁</div>');
 			$("#discoco_text").html('<div style="text-align: center;padding-top: 15px;padding-bottom: 10px;line-height:1.5em">歡迎參加「臺南購便利，行動支付So Easy」抽獎活動，請利用GOOGLE帳號登入，並填寫聯絡資料，即可擁有抽獎資格</div>');
 			$("#googleform").html('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRVhGlFuAcOjQ1fuRqMsLhj6Q2N_y7C1nCBOc_2BvyYSiRxg/viewform?embedded=true" width="640" height="500" frameborder="0" marginheight="0" marginwidth="0">載入中…</iframe>');
+			$("#pls1").html('<a href="https://docs.google.com/forms/d/e/1FAIpQLSeRVhGlFuAcOjQ1fuRqMsLhj6Q2N_y7C1nCBOc_2BvyYSiRxg/viewform?usp=sf_link">遇到註冊障礙?</a>');
+			
 			$("#submit").val('我要中大獎');
 			break;
 		case 'ds_shanhua_mp' :
 			$(".title_mainblk").html('<div style="text-align: center;padding-top: 28px;">「善化年中享樂購，商圈送你出國趣」抽獎註冊頁</div>');
 			$("#discoco_text").html('<div style="text-align: center;padding-top: 15px;padding-bottom: 10px;line-height:1.5em">歡迎參加「善化年中享樂購，商圈送你出國趣」抽獎活動，請利用GOOGLE帳號登入，並填寫聯絡資料，即可擁有抽獎資格</div>');
 			$("#googleform").html('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScuTxdz9TaeY28iSCPWwgRY2T3hGNS1M7bpri2XnSfduQhVYw/viewform?embedded=true" width="640" height="500" frameborder="0" marginheight="0" marginwidth="0">載入中…</iframe>');
+			$("#pls1").html('<a href="https://docs.google.com/forms/d/e/1FAIpQLScuTxdz9TaeY28iSCPWwgRY2T3hGNS1M7bpri2XnSfduQhVYw/viewform?usp=sf_link">遇到註冊障礙?</a>');
 			$("#submit").val('我要中大獎');
 			break;
 		default:
