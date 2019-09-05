4e25615b77a3f22b1d9d3eeaf82628d6d97e6bbd
cenm <cenm.lin@gmail.com>
2019-09-05 13:15:33 +0800

    new

--- efstore_help.js
+++ efstore_help.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function () {
 	var pagename = window.location.pathname.split('.jsp')[0].split('?')[0].split('/').pop();
-	//console.log(pagename);
+	console.log(pagename);
 	$('#register_shop_body').html("<div style=\"margin-bottom:20px;color:#CC0000;font-size:20px\">目前報名已截止，錄取名單公布於最新消息</div>");
+
 });
 
