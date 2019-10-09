81aa19351a9e07035020160723ac358309f39d3f
cenm <cenm.lin@gmail.com>
2019-10-09 12:49:17 +0800

    123

--- efstore_help.js
+++ efstore_help.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 	var pagename = window.location.pathname.split('.jsp')[0].split('?')[0].split('/').pop();
 	//console.log(pagename);
 	$('#register_shop_body').html("<div style=\"margin-bottom:20px;color:#CC0000;font-size:20px\">目前報名已截止，錄取名單公布於最新消息</div>");
-
+	$('#e_page').html("<div class=\"ic-title\"><a i18n='e_page'>EF stores</a></div><div class=\"btnBank-coBtn\"></div>");
 	if(pagename=="dialogue"){
 		setTimeout(function(){
 			addr_add();
